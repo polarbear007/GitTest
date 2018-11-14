27d91a3b6aeff864d1613b4216519c6c745d095d
eric <1479124685@qq.com>
2018-11-14 23:55:15 +0800

    add Teacher.java

---
+++ src/main/java/cn/itcast/entity/Teacher.java
@@ -0,0 +1,27 @@
+package cn.itcast.entity;
+
+public class Teacher {
+	private String name;
+	private int age;
+
+	public Teacher() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
